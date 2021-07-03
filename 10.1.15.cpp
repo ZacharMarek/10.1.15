fa20ce541ce1647188f0e52e39c4cbfa1c7afd7a
ZacharMarek <60191113+ZacharMarek@users.noreply.github.com>
2021-07-03 16:24:00 +0200

    Update 10.1.15.cpp

--- 10.1.15.cpp
+++ 10.1.15.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>  //odtialto potrebujem O_RDONLY na Ëitanie file
+#include <fcntl.h>  
 
 
 #define PIX(mat,x,y) (mat->px[(mat->height)*x+y])
@@ -18,15 +18,19 @@ GSI *gsi_create_empty(void)
 	GSI *mat; 
 	
 	mat = (GSI*)malloc(sizeof(GSI));
-	
+	if(mat==NULL)
+		return NULL;
+		
+	mat->height=0;
+	mat->width=0;	
 	return mat;	
 }
 
 GSI *gsi_create_with_geometry(unsigned int m, unsigned int n)
 {
 	GSI *mat;
-	
 	mat = (GSI*)malloc(sizeof(unsigned int)*m*n);
+	mat->px = (unsigned char*)malloc(sizeof(unsigned int)*m*n);	
 	
 	if(mat == NULL)
 		return NULL;
@@ -34,13 +38,8 @@ GSI *gsi_create_with_geometry(unsigned int m, unsigned int n)
 	mat->height = m;
 	mat->width = n;
 	
-	for(int i=0;i<m;i++)
-	{
-		for(int j =0;j<n;j++)
-		{
-		
-		}
-	}
+	if(mat->px=NULL)
+		return NULL;
 	
 	return mat;	
 	
@@ -50,7 +49,7 @@ GSI *gsi_create_with_geometry(unsigned int m, unsigned int n)
 GSI *gsi_create_with_geometry_and_color(unsigned int m, unsigned int n, unsigned char color) 
 {
 	GSI *mat;
-	
+	int i,j;
 	mat = (GSI*)malloc(sizeof(unsigned int)*m*n);
 	
 	if(mat == NULL)
@@ -58,14 +57,18 @@ GSI *gsi_create_with_geometry_and_color(unsigned int m, unsigned int n, unsigned
 	
 	mat->height = m;
 	mat->width = n;
-	
 	mat->px=(unsigned char*)malloc(sizeof(unsigned int)*m*n);
 	
-	for(int i=0;i<m;i++)
+	if(mat->px==NULL)
 	{
-		for(int j =0;j<n;j++)
+		free(mat->px);
+		return NULL;
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<m;j++)
 		{
-			PIX(mat,m,n)=color;
+			PIX(mat,i,j)=color;
 		}
 	}
 	
@@ -77,58 +80,125 @@ void gsi_destroy(GSI *mat)
 	free(mat);
 }
 
-GSI *gsi_create_by_pgm5(char *file_name) //naËitanie z file dorobiù
+GSI *gsi_create_by_pgm5(char *file_name) 
 {
 	GSI *mat;
-	char *format,*dat;
-	int fr = open(file_name, O_RDONLY);
-	int x,y;
-	//if ((fr = open(file_name, O_RDONLY) == NULL)
-	if(fr ==NULL)
-	{
-		//printf("subor sa neda optvoriù");
+	FILE* f;
+	
+	char line[30];
+	unsigned int sirka,vyska;
+	int i, j, col;
+	
+	f = fopen(file_name, "rb");
+	if(f == NULL)
 		return NULL;
-	}
 		
-	read(fr,format,2);	//najprv overime Ëi je  na zaËiatku P5
+	fgets(line, 10, f);
 	
-	if(format[0]!= 'P'  || format[1]!= '5')
-		return NULL;
+	if(line[0] != 'P' || line[1] != '5')	//subor musi zaƒçina≈• P5
+	{
+		fclose(f);
+		return 0;
+	}
 	
-	read(fr,dat,5);  //naËit data do matice
+	do
+	{
+		fgets(line, 80, f);
+	}
+	while(line[0] == '#');
+		sscanf(line, "%u%u", &sirka, &vyska);
 	
-	mat->width=dat[0];
-	mat->height=dat[2];
+	if((mat = gsi_create_with_geometry(sirka,vyska)) == NULL){
+		fclose(f);
+		return 0;
+	}
 	
-	PIX(mat,x,y)= read(fr,dat,mat->height*mat->width);
+	for(i = 0; i < mat->height; i++){
+		for(j = 0; j < mat->width; j++){
+			fscanf(f, "%c", &col);
+			PIX(mat, i, j) = col;
+		}
+	}
+	
+	if(fclose(f) == EOF){
+		return 0;
+	}
 	
 	return mat;
 }
 
-char gsi_save_as_pgm5(GSI *img, char *file_name, char *comment) //dorobiù
+char gsi_save_as_pgm5(GSI *mat, char *file_name, char *comment) 
 {
+	FILE *f;
+	int max = 0;
+	int i, j;
 	
+	f = fopen(file_name, "wb");
+	
+	if(f == NULL){
+		return 0;
+	}
+
+	fprintf(f,"P5\n");
+	
+	if(comment != NULL){
+		fprintf(f, "#");
+		fprintf(f, comment);
+		fprintf(f, "\n");
+	}
+	
+	for(i = 0; i < mat->height; i++){
+		for(j = 0; j < mat->width; j++){
+			
+			if(PIX(mat, i, j) > max)
+			max = PIX(mat, i, j);
+			
+		}
+	}
+	
+	fprintf(f,"%u %u %d\n", mat->width, mat->height, max);
+	
+	for(i = 0; i < mat->height; i++){
+		for(j = 0; j < mat->width; j++){
+			fprintf(f,"%c", PIX(mat, i, j));
+		}
+	}	
+	
+	return 1;
 }
 
 
-//budem to robiù tak ûe obehnem kaûdy jeden prvok matice a bud mu hodnotu zv˝öim alebo znÌûim podla argumentu br
-//treba si asi uvedomiù iba jedinu vec a to ûe ak ma nejaky prvok hodnutu 254 a chceme ju zv˝öiù o 2 tak tak jej hodnota bude sstale max 255
-//toto iste aj na opaËnom konci intervalu
+//budem to robi≈• tak ≈æe obehnem ka≈ædy jeden prvok matice a bud mu hodnotu zv√Ω≈°im alebo zn√≠≈æim podla argumentu br
+//treba si asi uvedomi≈• iba jedinu vec a to ≈æe ak ma nejaky prvok hodnutu 254 a chceme ju zv√Ω≈°i≈• o 2 tak tak jej hodnota bude stale max 255
+//toto iste aj na opaƒçnom konci intervalu
 void gsi_adjust_contrast(GSI *mat, float br)
 {
 	int i,j;
+	int max = 255;
+	int min = 0;
 	
 	
 	for(i=0;i<mat->height;i++)
 	{
 		for(j=0;j<mat->width;j++)
 		{
-			PIX(mat,i,j) += br;
-			
-			if (PIX(mat,i,j) > 255)
-				PIX(mat,i,j) =255;
-			if (PIX(mat,i,j) < 0)
-				PIX(mat,i,j) =0;	
+			if(br>0)
+			{
+			if(PIX(mat, i, j)+br > max)
+				PIX(mat, i, j) = max;
+			else
+				PIX(mat, i, j) += br;
+			}	
+			else if(br<0)
+			{
+			if(PIX(mat, i, j)+br < min)
+				PIX(mat, i, j) = min;
+			else
+				PIX(mat, i, j) += br;
+			}
+			else
+					PIX(mat, i, j);
+				
 			
 		}
 	}
@@ -143,7 +213,7 @@ void mat_print(GSI *mat)
 	{
 		for (j=0;j<mat->width;j++)
 		{
-			printf("%3.0f",PIX(mat,i,j));
+			printf("%d ",PIX(mat,i,j));
 		}
 		printf("\n");
 	}
@@ -152,14 +222,22 @@ void mat_print(GSI *mat)
 
 main()
 {
-	GSI *mat;
-	mat=gsi_create_with_geometry_and_color(7,3,60);
+	GSI *mat, *obrazok;
+	float br = -30;  //posun kontrastu
 	
-	float br = 10;  //posun kontrastu
+	mat=gsi_create_with_geometry_and_color(7,7,2);
+	mat_print(mat);
 	
 	gsi_adjust_contrast(mat,br);
 	mat_print(mat);
 	
+	/*
+	obrazok=gsi_create_by_pgm5("sunset.pgm");
+	gsi_adjust_contrast(obrazok,br);
+	save_as_pgm5(obrazok,"susnet_2.pgm", "pgm5")
+	
+	gsi_destroy(obrazok);
+	*/
 	
 	
 	gsi_destroy(mat);
